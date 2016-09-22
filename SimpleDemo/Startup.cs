c59be2d1ae602094d40ef495b4d3dcaf99708216
Rich Churcher <rich.churcher@gmail.com>
2016-09-22 14:26:42 +1200

    Add project files.

---
+++ SimpleDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleDemo.Startup))]
+namespace SimpleDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
